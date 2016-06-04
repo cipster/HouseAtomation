6e33380cdfe46afea8b5993e4d77c491601f5b77
ciprian <bostanciprian@gmail.com>
2016-06-05 00:35:16 +0300

    more refactorings

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 require.config({
     paths: {
-        "jquery": "bower_components/jquery/dist/jquery.min",
-        "bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min",
-        "svg": "bower_components/jquery-svg/jquery.svg.min",
-        "mustache": "bower_components/mustache.js/mustache.min"
+        jquery: "bower_components/jquery/dist/jquery.min",
+        bootstrap: "bower_components/bootstrap/dist/js/bootstrap.min",
+        svg: "bower_components/jquery-svg/jquery.svg.min",
+        mustache: "bower_components/mustache.js/mustache.min",
+        domReady: "bower_components/domReady/domReady",
+        HouseController: "lib/modules/controller/houseController",
+        EventService: "lib/modules/service/eventService",
+        StateService: "lib/modules/service/stateService",
+        TemplateService: "lib/modules/service/templateService",
+        LightService: "lib/modules/service/lightService",
+        CurtainService: "lib/modules/service/curtainService",
+        TemperatureService: "lib/modules/service/temperatureService"
     },
     shim: {
         "bootstrap": {"deps": ['jquery']}
     }
 });
 
-require(['lib/modules/controller/automationController'], function (automationController) {
-
+require(['HouseController'], function (HouseController) {
 });
